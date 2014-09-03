5f5014a1fcf9a8293c6ec11cb1737b168bf868c8
Josh Bassett <josh.bassett@gmail.com>
2014-09-03 11:13:40 +1000

    Don't scope timer functions to window object.
    This was broken in nodejs the timer functions are defined on the global object.

--- src/Control/Reactive/Timer.purs
+++ src/Control/Reactive/Timer.purs
@@ -7,48 +7,48 @@ foreign import data Timer     :: !
 foreign import data Timeout   :: *
 foreign import data Interval  :: *
 
-foreign import timeout 
-  "function timeout(time){                     \
-  \   return function(fn){                     \
-  \     return function(){                     \
-  \       return window.setTimeout(function(){ \
-  \         fn();                              \
-  \       }, time);                            \
-  \     };                                     \
-  \   };                                       \
-  \ }" :: forall a eff. 
-          Number ->  
-          (Eff (timer :: Timer | eff) a) -> 
-          Eff (timer :: Timer | eff) Timeout
+foreign import timeout
+  "function timeout(time) {\
+  \  var env = typeof global !== 'undefined' ? global : window;\
+  \  return function(fn) {\
+  \    return function() {\
+  \      return env.setTimeout(fn, time);\
+  \    };\
+  \  };\
+  \}" :: forall a eff.
+         Number ->
+         (Eff (timer :: Timer | eff) a) ->
+         Eff (timer :: Timer | eff) Timeout
 
-foreign import clearTimeout 
-  "function clearTimeout(timer){\
-  \   return function(){\
-  \     return window.clearTimeout(timer);\
-  \   };\
-  \ }" :: forall eff. 
-          Timeout -> 
-          Eff (timer :: Timer | eff) Unit
+foreign import clearTimeout
+  "function clearTimeout(timer) {\
+  \  var env = typeof global !== 'undefined' ? global : window;\
+  \  return function() {\
+  \    return env.clearTimeout(timer);\
+  \  };\
+  \}" :: forall eff.
+         Timeout ->
+         Eff (timer :: Timer | eff) Unit
 
 foreign import interval
-  "function interval(time){                     \
-  \   return function(fn){                      \
-  \     return function(){                      \
-  \       return window.setInterval(function(){ \
-  \         fn();                               \
-  \       }, time);                             \
-  \     };                                      \
-  \   };                                        \
-  \ }" :: forall a d eff.
-          Number ->
-          (Eff (timer :: Timer | eff) a) ->
-          Eff (timer :: Timer | eff) Interval
+  "function interval(time) {\
+  \  var env = typeof global !== 'undefined' ? global : window;\
+  \  return function(fn) {\
+  \    return function() {\
+  \      return env.setInterval(fn, time);\
+  \    };\
+  \  };\
+  \}" :: forall a d eff.
+         Number ->
+         (Eff (timer :: Timer | eff) a) ->
+         Eff (timer :: Timer | eff) Interval
 
 foreign import clearInterval
-  "function clearInterval(timer){\
-  \   return function(){\
-  \     return window.clearInterval(timer);\
-  \   };\
-  \ }" :: forall eff. 
-          Interval -> 
-          Eff (timer :: Timer | eff) Unit
+  "function clearInterval(timer) {\
+  \  var env = typeof global !== 'undefined' ? global : window;\
+  \  return function() {\
+  \    return env.clearInterval(timer);\
+  \  };\
+  \}" :: forall eff.
+         Interval ->
+         Eff (timer :: Timer | eff) Unit
