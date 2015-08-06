b4e1c457daa9e6fb82af69b3a9774dd6ea331a79
Legokichi Duckscallion <legokichi@duxca.com>
2015-08-07 01:20:51 +0900

    fix

--- test/Test.Main.purs
+++ test/Test.Main.purs
@@ -3,24 +3,24 @@ module Test.Main where
 import Prelude
 import Control.Monad.Eff (Eff())
 import Control.Monad.Eff.Console (print, CONSOLE())
-import Control.Monad.Eff.Exception(throwException, error, EXCEPTION())
+import Control.Monad.Eff.Exception (throwException, error, EXCEPTION())
 import Control.Timer
 
-main :: forall a. Eff (console :: CONSOLE, timer :: Timer,  err :: EXCEPTION | a) Unit
+main :: forall eff. Eff (console :: CONSOLE, timer :: Timer, err :: EXCEPTION | eff) Unit
 main = do
   print 1
-  t <- timeout 10 $ do
-    print 5
+  a <- timeout 10 $ do
+    print 4
   print 2
-  f <- timeout 0 $ do
-    throwException $ error "clearTimeout not working"
-  clearTimeout f
-  delay 20 print 6
+  delay 20 print 5
   print 3
-  g <- interval 20 $ do
-    print 7
-  delay 30 clearInterval g
-  print 4
+  b <- interval 30 $ do
+    print 6
+  delay 40 clearInterval b
+  c <- timeout 0 $ throwException $ do
+    error "clearTimeout is not working"
+  clearTimeout c
+  d <- interval 0 $ throwException $ do
+    error "clearInterval is not working"
+  clearInterval d
   return unit
-  where
-    lst = Nil
