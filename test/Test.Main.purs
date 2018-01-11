f93598fac92967db9e29c2f885aeb88aa83cdaa5
Jorge Acereda <jacereda@gmail.com>
2018-01-11 09:23:36 +0100

    Updates for 0.11

--- test/Test.Main.purs
+++ test/Test.Main.purs
@@ -1,26 +1,26 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (Eff())
-import Control.Monad.Eff.Console (print, CONSOLE())
-import Control.Monad.Eff.Exception (throwException, error, EXCEPTION())
-import Control.Monad.Eff.Timer
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (logShow, CONSOLE)
+import Control.Monad.Eff.Exception (throwException, error, EXCEPTION)
+import Control.Monad.Eff.Timer (TIMER, clearInterval, clearTimeout, delay, interval, timeout)
 
-main :: forall eff. Eff (console :: CONSOLE, timer :: TIMER, err :: EXCEPTION | eff) Unit
+main :: forall eff. Eff (console :: CONSOLE, timer :: TIMER, exception :: EXCEPTION | eff) Unit
 main = do
-  print 1
+  logShow 1
   a <- timeout 10 $ do
-    print 4
-  print 2
-  delay 20 print 5
-  print 3
+    logShow 4
+  logShow 2
+  _ <- delay 20 logShow 5
+  logShow 3
   b <- interval 30 $ do
-    print 6
-  delay 40 clearInterval b
+    logShow 6
+  _ <- delay 40 clearInterval b
   c <- timeout 0 $ throwException $ do
     error "clearTimeout is not working"
   clearTimeout c
   d <- interval 0 $ throwException $ do
     error "clearInterval is not working"
   clearInterval d
-  return unit
+  pure unit
